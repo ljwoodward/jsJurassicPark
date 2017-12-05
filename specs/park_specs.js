9494cba01d467357df3d4f97930a67a90d5f80ae
Laurence <ljwoodward.code@gmail.com>
2017-12-05 21:43:12 +0000

    refactored tests to avoid retyping data

--- specs/park_specs.js
+++ specs/park_specs.js
@@ -6,16 +6,24 @@ describe('Park', function () {
 
 });
 
+    const park = new Park();
+    const dino1 = new Dinosaur('T-Rex', 4);
+    const dino2 = new Dinosaur('T-Rex', 3);
+    const dino3 = new Dinosaur('Pterodactyl', 8);
+
+
 it('enclosure should start empty', function () {
-  const park = new Park();
   const actual = park.enclosure.length;
   assert.strictEqual(0, actual);
 }) ;
 
 it('should be able to add dinosaur', function () {
-  const dino = new Dinosaur('T-Rex', 4);
-  const park = new Park();
-  park.addDinosaurToEnclosure(dino);
+
+  park.addDinosaurToEnclosure(dino1);
   const actual = park.enclosure[0];
-  assert.strictEqual(dino, actual);
+  assert.strictEqual(dino1, actual);
 });
+
+it('should be able to remove all dinosaurs of a particular type', function () {
+
+})
