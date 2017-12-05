aaa10090b551df41efdb11d3f60b7cf54c250f41
Laurence <ljwoodward.code@gmail.com>
2017-12-05 21:32:41 +0000

    passed second test

--- park.js
+++ park.js
@@ -1,6 +1,11 @@
 const Park = function () {
   this.enclosure = [];
-}
+   this.addDinosaurToEnclosure = function (dinosaur) {
+      this.enclosure.push(dinosaur);
+   };
+};
+
+
 
 
 module.exports = Park;
