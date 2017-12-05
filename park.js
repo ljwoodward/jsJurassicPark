f30cf14d4b2b2777fdb0d1a847bbf32177987688
Laurence <ljwoodward.code@gmail.com>
2017-12-05 22:04:37 +0000

    passed third test

--- park.js
+++ park.js
@@ -1,7 +1,15 @@
 const Park = function () {
   this.enclosure = [];
    this.addDinosaurToEnclosure = function (dinosaur) {
-      this.enclosure.push(dinosaur);
+      this.enclosure.unshift(dinosaur);
+   };
+   this.removeAllOfType = function(type) {
+     for (var i = 0; i < this.enclosure.length; i++) {
+       if (this.enclosure[i].type == type) {
+         this.enclosure.splice(1, i);
+         this.removeAllOfType(type);
+       }
+     }
    };
 };
 
--- specs/park_specs.js
+++ specs/park_specs.js
@@ -10,20 +10,26 @@ describe('Park', function () {
     const dino1 = new Dinosaur('T-Rex', 4);
     const dino2 = new Dinosaur('T-Rex', 3);
     const dino3 = new Dinosaur('Pterodactyl', 8);
+    park.addDinosaurToEnclosure(dino1);
+    park.addDinosaurToEnclosure(dino2);
+    park.addDinosaurToEnclosure(dino3);
 
 
 it('enclosure should start empty', function () {
-  const actual = park.enclosure.length;
+  const newPark = new Park();
+  const actual = newPark.enclosure.length;
   assert.strictEqual(0, actual);
 }) ;
 
 it('should be able to add dinosaur', function () {
-
-  park.addDinosaurToEnclosure(dino1);
+  const newDino = new Dinosaur('Diplodocus', 3);
+  park.addDinosaurToEnclosure(newDino);
   const actual = park.enclosure[0];
-  assert.strictEqual(dino1, actual);
+  assert.strictEqual(newDino, actual);
 });
 
 it('should be able to remove all dinosaurs of a particular type', function () {
-
-})
+  park.removeAllOfType('T-Rex');
+  const actual = park.enclosure.length;
+  assert.strictEqual(1, actual);
+});
